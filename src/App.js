bb22b0a86119d011b58f7eabd0aad72f541b7e79
gyeongjae cheon <yocasd2@gmail.com>
2022-05-25 23:42:24 +0900

    작업 메뉴얼, json 파일 생성

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import React, { memo } from 'react';
-import MapContainer from './components/MapContainer';
+import React, { memo } from "react";
+import MapContainer from "./components/MapContainer";
 
 const App = memo(() => {
-  return (
-    <div>
-      <MapContainer />
-    </div>
-  );
+  return <MapContainer />;
 });
 
 export default App;
--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -3,7 +3,32 @@ import styled from "styled-components";
 
 const MapView = styled.div`
   width: 100%;
-  height: 920px;
+  height: 100%;
+  .map {
+    width: 500px;
+    height: 500px;
+  }
+  .m {
+    display: flex;
+    .a {
+      width: 100px;
+      height: 100px;
+      background-color: aliceblue;
+      border: 1px solid black;
+    }
+    .b {
+      width: 100px;
+      height: 100px;
+      background-color: #0280e0;
+      border: 1px solid black;
+    }
+    .c {
+      width: 100px;
+      height: 100px;
+      background-color: #04c1de;
+      border: 1px solid black;
+    }
+  }
 `;
 
 // window 전역 객체로 들어간 kakao 객체 변수로 뽑기.
@@ -11,9 +36,9 @@ const { kakao } = window;
 
 const MapContainer = memo(() => {
   const myMap = useRef();
-  const container = myMap.current;
- 
+
   useEffect(() => {
+    const container = myMap.current;
     const options = {
       center: new kakao.maps.LatLng(37.5026, 127.0249),
       level: 3,
@@ -25,10 +50,17 @@ const MapContainer = memo(() => {
     // map에 컨트롤러 추가, 컨트롤러 위치는 오른쪽이다.
     const zoomControl = new kakao.maps.ZoomControl();
     map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
-  }, [container]);
+  }, []);
 
   return (
-    <MapView ref={myMap}>
+    <MapView>
+      <div className="map" ref={myMap}></div>
+      <br/><br/>
+      <div className="m">
+        <div className="a"></div>
+        <div className="b"></div>
+        <div className="c"></div>
+      </div>
     </MapView>
   );
 });
