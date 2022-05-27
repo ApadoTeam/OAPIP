930ec5c70b37375a3960fe0a361a802986148388
gyeongjae cheon <yocasd2@gmail.com>
2022-05-28 03:11:30 +0900

    .

--- src/App.js
+++ src/App.js
@@ -4,23 +4,17 @@ import MapContainer from "./pages/MapContainer";
 import Login from "./components/Login";
 import Join from "./components/Join";
 
-import styled from "styled-components";
-
-const AppCss = styled.div`
-  
-`;
-
 
 
 const App = memo(() => {
   return (
-    <AppCss>
+    <>
       <Routes>
         <Route path="/" exact element={<Login />} />
         <Route path="/signup" element={<Join />} />
         <Route path="mapcontainer" element={<MapContainer />} />
       </Routes>
-    </AppCss>
+    </>
   );
 });
 
