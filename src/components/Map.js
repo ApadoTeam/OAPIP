8142e1df7c8d074851ceeebf7fb7f13f40d32854
SeungaLeeah <92766716+SeungaLeeah@users.noreply.github.com>
2022-05-28 21:20:54 +0900

    map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,4 +1,4 @@
-import React, { memo, useEffect, useRef } from "react";
+import React, { memo, useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 
 const MapView = styled.div`
@@ -8,11 +8,14 @@ const MapView = styled.div`
   top: 0;
   right: 0;
 `;
+const MapMarker= styled.div`
+
+`;
 
 // window 전역 객체로 들어간 kakao 객체 변수로 뽑기.
 const { kakao } = window;
 
-const MapContainer = memo(() => {
+const Map = memo(() => {
   const myMap = useRef();
 
   useEffect(() => {
@@ -30,12 +33,54 @@ const MapContainer = memo(() => {
     map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
   }, []);
 
-  return (
-    <>
-      <MapView ref={myMap}>
-      </MapView>
-    </>
-  );
-});
+  const [info, setInfo] = useState()
+  const [markers, setMarkers] = useState([])
+  const [map, setMap] = useState()
 
-export default MapContainer;
+  useEffect(() => {
+    if (!map) return
+    const ps = new kakao.maps.services.Places()
+
+    ps.keywordSearch("이태원 맛집", (data, status, _pagination) => {
+      if (status === kakao.maps.services.Status.OK) {
+        // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
+        // LatLngBounds 객체에 좌표를 추가합니다
+        const bounds = new kakao.maps.LatLngBounds()
+        let markers = []
+
+        for (var i = 0; i < data.length; i++) {
+          markers.push({
+            position: {
+              lat: data[i].y,
+              lng: data[i].x,
+            },
+            content: data[i].place_name,
+          })
+          bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x))
+        }
+        setMarkers(markers)
+
+        // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
+        map.setBounds(bounds)
+      }
+    })
+  }, [map])
+
+  return (
+  <>
+  <MapView ref={myMap}></MapView>
+  {markers.map((marker) => (
+        <MapMarker
+          key={`marker-${marker.content}-${marker.position.lat},${marker.position.lng}`}
+          position={marker.position}
+          onClick={() => setInfo(marker)}
+        >
+          {info &&info.content === marker.content && (
+            <div style={{color:"#000"}}>{marker.content}</div>
+          )}
+        </MapMarker>
+      ))}
+  </>
+  )
+})
+export default Map;
