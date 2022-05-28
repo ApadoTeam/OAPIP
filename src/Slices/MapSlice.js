22ec47a456f4fdd3e437374372c9047eb9a71c0e
chanCo <loinsective@naver.com>
2022-05-29 05:12:28 +0900

    local API

--- src/Slices/MapSlice.js
+++ src/Slices/MapSlice.js
@@ -1,12 +1,22 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from 'axios';
 
+const API_URL = 'https://dapi.kakao.com/v2/local/search/address.json';
+const API_KEY = 'bb10b2a5cc7da58fb075c6b428ff9ed1';
+
 export const getMapInfo = createAsyncThunk('map/getMapInfo', async (payload, { rejectWithValue }) => {
   let result = null;
 
   try {
-    result = await axios.get(`https://dapi.kakao.com/v2/local/search/address.json`, {
-      header: { Authorization: '8dd322609444f19d7248550cf5fba7a2' },
+    result = await axios.get(API_URL, {
+      params: {
+        query: payload.query ? payload.query : '강남구',
+        analyze_type: payload.analyze_type ? payload.analyze_type : 'similar',
+        page: payload.page ? payload.page : 1,
+        size: payload.size ? payload.size : 20,
+      },
+
+      headers: { Authorization: `KakaoAK ${API_KEY}` },
   });
   } catch (err) {
     result = rejectWithValue(err.response);
@@ -19,6 +29,10 @@ const MapSlice = createSlice({
   name: 'map',
   initialState: {
     toggle: +true,
+    meta: null,
+    documents: null,
+    loading: false,
+    error: null,
   },
   reducers: {
     toggleBtn: (state, action) => {
@@ -36,14 +50,16 @@ const MapSlice = createSlice({
     },
     [getMapInfo.fulfilled]: (state, { payload }) => {
       return {
-        data: payload?.data,
+        meta: payload?.data?.meta,
+        documents: payload?.data?.documents,
         loading: false,
         error: null
       }
     },
     [getMapInfo.rejected]: (state, { payload }) => {
       return {
-        data: payload?.data,
+        meta: null,
+        documents: null,
         loading: false,
         error: {
           code: payload?.status ? payload.status : 500,
