70f04e931b84657bc722fce91ec08697ea857f5b
gyeongjae cheon <yocasd2@gmail.com>
2022-05-29 04:21:19 +0900

    relayout

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 //import MapContainer from "./pages/MapContainer";
 import Login from "./components/Login";
 import Join from "./components/Join";
-import Map from "./components/Map";
+import Map from "./pages/Map";
 
 
 
--- src/pages/Map.js
+++ src/pages/Map.js
@@ -10,7 +10,7 @@ const MapCss = styled.div`
   display: flex;
   flex-direction: row;
   .map {
-    width: 100vw;
+    width: 100%;
     height: 100vh;
     background-color: gray;
     display: flex;
@@ -26,6 +26,8 @@ const MapCss = styled.div`
       border-left: 0;
       border-top-right-radius: 5px;
       border-bottom-right-radius: 5px;
+      z-index: 99;
+
       .left {
         display: block;
         background: url(${arrowL}) no-repeat;
@@ -33,48 +35,52 @@ const MapCss = styled.div`
         background-position: -7px;
         text-indent: -99999px;
       }
-      .false {
+      .right {
         display: block;
-        width: 20px;
-        height: 20px;
         background: url(${arrowR}) no-repeat;
-        background-size: 20px 20px;
-        margin: 0 auto;
+        background-size: 25px 40px;
+        background-position: -7px;
         text-indent: -99999px;
       }
     }
   }
 `;
 
-// window 전역 객체로 들어간 kakao 객체 변수로 뽑기.
-  // const { kakao } = window;
+  // window 전역 객체로 들어간 kakao 객체 변수로 뽑기.
+  const { kakao } = window;
+
 const Map = memo(() => {
-  // const myMap = useRef();
-  // useEffect(() => {
-  //   // 지도를 렌더링할 영역을 ref로 뽑아 변수에 담기
-  //   const container = myMap.current;
-  //   // 지도 생성 시 옵션 (위치, 지도 확대 레벨)
-  //   const options = {
-  //     center: new kakao.maps.LatLng(37.5026, 127.0249),
-  //     level: 3,
-  //     tileAnimation: true,
-  //   };
-  //   const map = new kakao.maps.Map(container, options);
-  //   // map에 컨트롤러 추가, 컨트롤러 위치는 오른쪽이다.
-  //   const zoomControl = new kakao.maps.ZoomControl();
-  //   map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
-  //   console.log(container.width);
-  // }, []);
-
+  const myMap = React.useRef();
   const { toggle } = useSelector((state) => state.map);
-  const dispatch = useDispatch();
 
+  React.useEffect(() => {
+    // 지도를 렌더링할 영역을 ref로 뽑아 변수에 담기
+    const container = myMap.current;
+    // 지도 생성 시 옵션 (위치, 지도 확대 레벨)
+    const options = {
+      center: new kakao.maps.LatLng(37.5026, 127.0249),
+      level: 3,
+      tileAnimation: true,
+    };
+    const map = new kakao.maps.Map(container, options);
+    // map에 컨트롤러 추가, 컨트롤러 위치는 오른쪽이다.
+    const zoomControl = new kakao.maps.ZoomControl();
+    map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+    if (toggle === false) {
+      map.ralayout();
+    }
+  }, [toggle]);
+
+  
+  
+  const dispatch = useDispatch();
+console.log(toggle);
   return (
       <MapCss>
         <SideBar />
-        <div className="map">
+        <div className="map" ref={myMap}>
           <button className='sideBtn' onClick={(e) => { dispatch(toggleBtn(+false))}}>
-          {toggle ? <span className="left">left</span> : <span className="left">right</span>}
+          {toggle ? <span className="left">left</span> : <span className="right">right</span>}
           </button>
         </div>
       </MapCss>
